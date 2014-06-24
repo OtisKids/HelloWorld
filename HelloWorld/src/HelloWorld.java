5cdedd94980f0c17ba988f89f383cb0a719d8132
Greg <g.butheau@gmail.com>
2014-06-24 19:04:26 +0200

    Initial checkin of HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,54 @@
+/**
+ * Hello World
+ */
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+/**
+ * @author Greg
+ *
+ */
+
+public class HelloWorld extends JFrame{
+
+	   /*private JTextField item1;
+	   private JTextField item2;
+	   private JTextField item3;
+	   private JPasswordField passwordField;
+	   Container contentPane ;
+	   
+	   public HelloWorld(){
+	      super("The title");
+
+	      setLayout(new FlowLayout());
+
+	      item1 = new JTextField(10);
+	      contentPane.add(item1);
+
+	      item2 = new JTextField("enter text here");
+	      add(item2);
+
+	      item3 = new JTextField("uneditable", 20);
+	      item3.setEditable(false);
+	      add(item3);
+
+	      passwordField = new JPasswordField("mypass");
+	      add(passwordField);
+
+	      thehandler handler = new ();
+
+	      item1.addActionListener(handler);
+	      item2.addActionListener(handler);
+	      item3.addActionListener(handler);
+	      passwordField.addActionListener(handler);*/
+	   
+
+	   public static void main(String[] args){
+	     System.out.println("toto");
+	   }
+	}
